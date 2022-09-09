4c4243b3d10cd595d1013b88762457f6df05eb69
Prince <princekizhthara@gmail.com>
2022-09-09 13:47:54 +0530

    feat(menu): add responsiveness

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import BlogsIcon from "./icons/blogs";
 import HomeIcon from "./icons/home";
 import ContactIcon from "./icons/menu";
@@ -74,52 +74,94 @@ const hoverDraw = {
   },
 };
 
+const toggleDrawer = {
+  show: {
+    left: 15,
+  },
+  hide: {
+    left: -50,
+  },
+};
+
 export default function Menu() {
   const [hoverState, setHoverState] = useState<boolean[]>(
     svgs.map((_) => false)
   );
+  const [toggle, setToggle] = useState<boolean>(false);
   const router = useRouter();
+
+  useEffect(() => {
+    setToggle(false);
+  }, [router.asPath]);
+  
   return (
-    <div className={classes.Container}>
-      {svgs.map((svg, index) => {
-        const isCurrentPath = router.pathname == svg.path;
-        return (
-          <Fragment key={`menu_item_${index}`}>
-            <Link href={svg.path}>
-              <motion.div
-                className={[
-                  classes.Menu,
-                  isCurrentPath ? "" : classes.Active,
-                ].join(" ")}
-                initial="hidden"
-                animate="visible"
-                variants={menuDraw}
-                custom={index}
-                onMouseEnter={() =>
-                  setHoverState(hoverState.map((_, i) => i == index))
-                }
-                onMouseLeave={() => setHoverState(hoverState.map(() => false))}
-              >
+    <>
+      <div
+        className={classes.Burger}
+        onClick={() => setToggle((prev) => !prev)}
+      >
+        <svg
+          width="8"
+          height="8"
+          viewBox="0 0 8 8"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 0H8V0.8H0V0ZM0 3.6H8V4.4H0V3.6ZM0 7.2H8V8H0V7.2Z"
+            fill="#121212"
+          />
+        </svg>
+      </div>
+      <motion.div
+        className={classes.Container}
+        variants={toggleDrawer}
+        animate={toggle ? "show" : "hide"}
+      >
+        {svgs.map((svg, index) => {
+          const isCurrentPath = router.pathname == svg.path;
+          return (
+            <Fragment key={`menu_item_${index}`}>
+              <Link href={svg.path}>
                 <motion.div
-                  animate={isCurrentPath || hoverState[index] ? "show" : "hide"}
-                  variants={hoverDraw}
-                  className={classes.Hover}
+                  className={[
+                    classes.Menu,
+                    isCurrentPath ? "" : classes.Active,
+                  ].join(" ")}
+                  initial="hidden"
+                  animate="visible"
+                  variants={menuDraw}
+                  custom={index}
+                  onMouseEnter={() =>
+                    setHoverState(hoverState.map((_, i) => i == index))
+                  }
+                  onMouseLeave={() =>
+                    setHoverState(hoverState.map(() => false))
+                  }
+                >
+                  <motion.div
+                    animate={
+                      isCurrentPath || hoverState[index] ? "show" : "hide"
+                    }
+                    variants={hoverDraw}
+                    className={classes.Hover}
+                  ></motion.div>
+                  <svg.icon selected={isCurrentPath || hoverState[index]} />
+                </motion.div>
+              </Link>
+              {svgs.length - 1 !== index && (
+                <motion.div
+                  className={classes.Line}
+                  variants={drawLine}
+                  initial="hidden"
+                  animate="visible"
+                  custom={index}
                 ></motion.div>
-                <svg.icon selected={isCurrentPath || hoverState[index]} />
-              </motion.div>
-            </Link>
-            {svgs.length - 1 !== index && (
-              <motion.div
-                className={classes.Line}
-                variants={drawLine}
-                initial="hidden"
-                animate="visible"
-                custom={index}
-              ></motion.div>
-            )}
-          </Fragment>
-        );
-      })}
-    </div>
+              )}
+            </Fragment>
+          );
+        })}
+      </motion.div>
+    </>
   );
 }
